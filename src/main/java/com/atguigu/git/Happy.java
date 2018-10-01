da691536b8c248535c483ec43c0e6b7c23b0c118
tom_glb <goodMorning_glb@atguigu.com>
2018-10-02 03:34:00 +0800

    commit test right

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -3,10 +3,8 @@ package com.atguigu.git;
 public class Happy {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("right...");
-=======
+
 		System.out.println("left...");
->>>>>>> branch 'master' of https://github.com/jackson2018hadoop/TestGit.git
 	}
 }
